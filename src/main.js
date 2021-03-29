61ffd5362817bd8bb5691a082a4f516134b57178
Mariano <avicomariano@gmail.com>
2021-03-29 12:58:47 -0300

    Método actualizar funcionando, falta el callback de loguear y sacar todo del main que utilicé para probar

--- src/main.js
+++ src/main.js
@@ -11,23 +11,81 @@ const rutaDeudasNew = './out/deudasNEW.json'
 const rutaLog = './out/notificaciones.log'
 
 //ap.actualizarArchivosDeudas(rutaDeudasOld, rutaPagos, rutaDeudasNew, rutaLog)
-function jsonToJS(ruta){
-    const pagos = fs.readFileSync(ruta);
-    const pagosJS = JSON.parse(pagos); 
 
-    //return pagosJS;
-   // console.log(pagosJS);
+
+function jsonToJS(ruta) {
+    const pagos = fs.readFileSync(ruta);
+    const pagosJS = JSON.parse(pagos);
     return pagosJS;
 }
 const pagosJS = jsonToJS('./in/pagos.json');
-
 const deudas = jsonToJS('./in/deudasOLD.json');
 
-console.log("Estas son las deudas ",deudas);
-console.log("Estos son los pagos ", pagosJS)
-/* const deudasJS = jsonToJS('./in/deudasO.json');
+//Esta sería la función actualizar de actualizarDeudas.js.
+function actualizar() {
 
-const arrCombinado = [];
+    //Acá guardaría todas las deudas actualizadas, que es el array 
+    //que debe devolver la función según los pagos.
+    const arrDeudasActualizadas = [];
 
-console.log(deudasJS)
-//console.log(pagosJS) */
+    //Esta variable va a guardar los dni de todos los pagos    
+    let dniPagoNoCoincide = [];
+
+    //Esta variable va a guardar los pagos y las deudas que se encuentren
+    // con dni pero no coincidan sus apellidos
+    const coincideDnIPeroNoApellido = [];
+
+    //Este array va a guardar todas las deudas actualizadas.
+    let deudasNew = [];
+
+    //Clientes con saldo a favor
+    let clientesConSaldoAFavor = [];
+
+    //Este for me va a cargar todos los pagos
+    pagosJS.forEach(pagos => {
+        dniPagoNoCoincide.push(pagos);
+    })
+    //Verifico cada pago con cada deuda.
+    deudas.forEach((deuda, x) => {
+        pagosJS.forEach((pago, i) => {
+            if (pago.dni === deuda.dni) {
+                //Si coincide el dni de la deuda con el del pago voy dejando en 0 esa posición del array 
+                //para descartarlo porque conincidió y así filtrar que quede solo el que no lo hizo. 
+                dniPagoNoCoincide[i] = null;
+                if (pago.apellido === deuda.apellido) {
+                    //Le resto el pago a la deuda
+                    console.log("deuda antes de restar: ", deuda.debe);
+                    console.log("pago a realizar: ", pago.pago);
+                    deuda.debe -= pago.pago
+                    console.log("deuda después de restar: ", deuda.debe);
+
+                    if (deuda.debe < 0) {
+                        //Me guardo los clientes que tienen saldo a favor
+                        clientesConSaldoAFavor.push(pago);
+                    } else {
+                        deudasNew.push(deuda)
+                    }
+                } else {
+                    //Creo un objeto para guardar las deudas y los pagos que 
+                    //coincidan en el dni pero no en el apellido y lo pusheo.
+                    let deudaPago = {};
+                    deudaPago.Deuda = deuda;
+                    deudaPago.pago = pago;
+                    coincideDnIPeroNoApellido.push(deudaPago);
+                    //Me guardo la deuda ya que no se pudo pagar por el problema del nombre
+                    //Y la tengo que mostrar como pendiente
+                    deudasNew.push(deuda);
+
+                }
+            }
+
+
+        });
+    });
+    
+    return deudasNew;
+}
+
+console.log(actualizar());
+
+
--- src/utils/actualizarDeudas.js
+++ src/utils/actualizarDeudas.js
@@ -12,32 +12,53 @@ import fs from 'fs'
  * @param {loggerCallback} logger funcion a la cual llamar en caso de necesitar loguear un evento
  * @returns {Object[]} las deudas actualizadas
  */
+
+/**
+ * Con el tema del log voy a tener que escribir en un documento con las erramientas esas fs...
+ * ● Si aparece un registro de pago con un dni que no coincide con el de ninguna deuda, ese
+registro no se procesa, y se debe loguear como operación inválida.ESTÁ
+
+● Si aparece un registro de pago que coincide con una deuda en su dni pero no en su
+apellido, el mismo no se procesa, y se deben loguear ambos, la deuda y el pago. ESTÁ
+
+ ●Si un registro de deuda no posee pagos asociados, se agrega directamente al array
+actualizado, sin cambios. FALTA
+
+ ●Si luego de aplicar todos los pagos correspondientes a una deuda, ésta aun
+queda en positivo, se agrega al array actualizado con el nuevo importe.
+
+
+
+ */
 function actualizar(deudas, pagos, logger) {
-    
+    const arrCombinado = [];
+    let dniPagoNoCoincide = [];
     const deudasJS = jsonToJS('./in/deudasO.json');
     const pagosJS = jsonToJS('./in/pagos.json');
-    const arrCombinado = [];
+    console.log(deudasJS);
+    console.log(pagosJS);
 
-    console.log(deudasJS)
-    console.log(pagosJS)
+    deudasJS.forEach((deuda, x) => {
+
+        pagosJS.forEach((pago, i) => {
+            if (deuda.dni) {
+
+            }
+        });
+    });
+
+
+
 }
-    //Loopear pagos
- 
- /*    Entrar al primer subindice del array de pagos y acceder al objeto.dni
-        Loopear deudas
-            Guardar el dni y buscarlo en el deudasOld
-            si se encuentra
-                copiar el objeto al nuevo array con la deuda actualizada
 
-*/
 
-function jsonToJS(ruta){
+function jsonToJS(ruta) {
     const pagos = fs.readFileSync(ruta);
-    const pagosJS = JSON.parse(pagos);   
+    const pagosJS = JSON.parse(pagos);
     return pagosJS;
 }
 
-export default{
+export default {
     actualizar,
     jsonToJS
 }
